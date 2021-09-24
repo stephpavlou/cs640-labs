21d177c2a4ab48c8433e7d64e1f9d220ddd944ee
cdupont2 <61169398+cdupont2@users.noreply.github.com>
2021-09-23 22:00:30 -0500

    Add files via upload

--- lab1/Iperfer.java
+++ lab1/Iperfer.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cs640_lab1;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -22,29 +15,26 @@ public class Iperfer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        run_client("",0,0);
-        //run_server(0);
-        String hostname_ip;
-        int port_num;
-        int time;
+        String hostname_ip = "";
+        int port_num = 0;
+        int time = 0;
         // TODO code application logic here
-        //if(!(args.length == 3 || args.length == 6)) {
-         //   System.out.println("Error: Invalid arguments");
-        //    System.exit(1);
-        //}
+        if(!(args.length == 3 || args.length == 7)) {
+            System.out.println("Error: Invalid arguments");
+            System.exit(1);
+        }
         if(args[0].equals("-c")) {
-            run_client("",0,0);
-            //if ((!args[1].equals("-h")) || (!args[3].equals("-p")) || (!args[5].equals("-t"))) {
-            //    System.out.println("Error: Invalid arguments");
-            //    System.exit(1);
-            //}
+            if ((!args[1].equals("-h")) || (!args[3].equals("-p")) || (!args[5].equals("-t"))) {
+                System.out.println("Error: Invalid arguments");
+                System.exit(1);
+            }
             // Check input values
             // Hostname
-            //try {
+            try {
                 
-            //} catch (Exception ex) {
+            } catch (Exception ex) {
                 
-            //}
+            }
             // Port
             try {
                 port_num = Integer.parseInt(args[4]);
@@ -68,10 +58,20 @@ public class Iperfer {
                 System.out.println("Error: time must be a valid number");
                 System.exit(1);
             }
-    
-            
+            run_client(hostname_ip, port_num, time);
         } else if(args[0].equals("-s")) {
-            run_server(0);
+            // Port
+            try {
+                port_num = Integer.parseInt(args[2]);
+                if((port_num < 1024) || (port_num > 65535)) {
+                    System.out.println("Error: port number must be in the range 1024 to 65535");
+                    System.exit(1);
+                }
+            } catch (Exception ex) {
+                System.out.println("Error: port number must be a valid number");
+                System.exit(1);
+            }
+            run_server(port_num);
         } else {
             System.out.println("Error: Invalid arguments");
             System.exit(1);
@@ -80,22 +80,27 @@ public class Iperfer {
     
     public static void run_client(String hostname_ip, int port_num, int time) {
         try{
-            String test_hostname_ip = "127.0.0.1";
-            int test_port_num = 5000;
-            int test_time = 3;
-            Socket socket = new Socket(test_hostname_ip, test_port_num);
+            int num_packets = 0;
+            double data_rate;
+            Socket socket = new Socket(hostname_ip, port_num);
+            System.out.println("Client has connected");
             PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-            String msg = "Hello World!";
+            //String msg = "Hello World!";
             byte barray[] = new byte[1000];
             for(int i = 0; i < 1000; i++) {
                 barray[i] = 0;
             }
             //ADD loop 
-            
-            out.println(msg);
+            long target_time = System.currentTimeMillis() + (1000 * time);
+            while(System.currentTimeMillis() < target_time) {
+                num_packets++;
+                out.println(barray);
+            }
             
             out.close();
             socket.close();
+            data_rate = ((num_packets*8000)/1000000)/time;
+            System.out.printf("sent=%d KB rate =%.3f Mbps", num_packets, data_rate);
         } catch(Exception ex) {
             
         }
@@ -103,12 +108,14 @@ public class Iperfer {
     
     public static void run_server(int port_num) {
         try{
-            int test_port_num = 5000;
-            ServerSocket server = new ServerSocket(test_port_num);
+            ServerSocket server = new ServerSocket(port_num);
             Socket socket = server.accept();
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String line = in.readLine();
-            System.out.println(line);
+            String line = "";
+            while((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            
             in.close();
             socket.close();
             server.close();
